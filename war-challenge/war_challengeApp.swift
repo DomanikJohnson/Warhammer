387eb8ff51b03225c76e13e045f45166279b0858
SwiftDom <domanik.johnson@icloud.com>
2021-05-27 22:10:58 -0400

    Added iOS project

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Domanik Johnson on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
